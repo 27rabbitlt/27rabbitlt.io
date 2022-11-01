8f6648b7bae15cd89dcfb66b7d5ea5d478bcf0e6
Tony Stark / Teng Liu <27rabbitlt@gmail.com>
2022-11-01 21:36:03 +0800

    update

--- help_build.py
+++ help_build.py
@@ -17,18 +17,22 @@ ayml["nav"] = [default]
 prefix = 'posts'
 dir = 'ALGO'
 
-print (os.path.join(prefix, dir))
+print (prefix + "/" + dir)
 
 for dir in dirs:
     dir_list = []
     dir_dict = {dir: dir_list}
-    for _, _, fs in os.walk(os.path.join(rt, dir)):
+    for _, _, fs in os.walk(rt + "/" + dir):
         break
     for file in fs:
         file_dict = {os.path.splitext(file)[0] \
             if os.path.splitext(file)[0] != "index"\
-            else dir: os.path.join("posts", dir, file)}
-        dir_list.append(file_dict)
+            else dir: "posts/" + dir + "/" + file}
+        if (file == "index.md"):
+            dir_list.insert(0, file_dict)
+        else:
+            dir_list.append(file_dict)
+            
     ayml["nav"].append(dir_dict)
 
 print (ayml["nav"])
